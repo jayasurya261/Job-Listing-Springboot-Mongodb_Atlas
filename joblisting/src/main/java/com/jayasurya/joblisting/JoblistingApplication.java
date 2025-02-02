c38b7f93de843c23810dc32023fb6222a2cd67e7
Jayasurya R <suryasunrise261@gmail.com>
2025-02-02 21:20:31 +0530

    code updated

--- joblisting/src/main/java/com/jayasurya/joblisting/JoblistingApplication.java
+++ joblisting/src/main/java/com/jayasurya/joblisting/JoblistingApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JoblistingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JoblistingApplication.class, args);
+		SpringApplication.run(JoblistingApplication.class, args) ;
 	}
 
 }
